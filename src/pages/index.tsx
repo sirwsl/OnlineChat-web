44138a3e8d207def2bb38f1338861c7370b1b0c5
王世磊 <sirwsl@163.com>
2021-07-13 21:50:49 +0800

    首页修改

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import {Card, Col, Row, Statistic} from 'antd';
+import {Card, Col, Divider, Row, Statistic} from 'antd';
 import styles from './index.less'
 import {ArrowUpOutlined} from "@ant-design/icons";
 import { history } from 'umi';
@@ -19,8 +19,18 @@ export default (): React.ReactNode => {
       <Row>
         <Col span={24}>
           <Card title="注意事项" className={styles.margin}>
-            <p>该系统基于AntD+springBoot+WebSocket进行开发</p>
-            <p>开发不易请务攻击</p>
+            <p>1、该系统基于AntD+springBoot+WebSocket进行开发</p>
+            <p>2、该系统仅用于学习、交流使用，不做其他用途，若在使用中出现问题，概不负责</p>
+            <p>3、在聊天过程中请文明聊天，不适用不当言论，一经发现严肃处理，若触及违法相关移交公安机关</p>
+            <p>4、开发不易请务攻击</p>
+            <Divider style={{margin:'10px 0'}}/>
+            <p>作者：sirwsl</p>
+            <p>开源地址：GitHub： 码云：</p>
+            <p>个人博客：<a href="https://www.wslhome.top">https://www.wslhome.top</a>
+              CSDN:<a href="https://blog.csdn.net/qq_40432886">sirwsl</a>
+            </p>
+            <p>其余项目: 商城（秒杀）：<a href="https://kill.wslhome.top">https://kill.wslhome.top</a>
+            管理界面：<a href="https://admin.wslhome.top">https://admin.wslhome.top</a></p>
           </Card>
         </Col>
 
@@ -29,7 +39,6 @@ export default (): React.ReactNode => {
               <Statistic
                 title="在线人数"
                 value={messageData}
-                precision={2}
                 valueStyle={{ color: '#3f8600' }}
                 prefix={<ArrowUpOutlined />}
                 suffix="人"
@@ -40,8 +49,7 @@ export default (): React.ReactNode => {
           <Card>
             <Statistic
               title="今日活跃"
-              value={11.28}
-              precision={2}
+              value={11}
               valueStyle={{ color: '#3f8600' }}
               prefix={<ArrowUpOutlined />}
               suffix="人"
@@ -52,8 +60,7 @@ export default (): React.ReactNode => {
           <Card>
             <Statistic
               title="本周活跃"
-              value={11.28}
-              precision={2}
+              value={11}
               valueStyle={{ color: '#3f8600' }}
               prefix={<ArrowUpOutlined />}
               suffix="人"
@@ -64,8 +71,7 @@ export default (): React.ReactNode => {
           <Card>
             <Statistic
               title="本月活跃"
-              value={11.28}
-              precision={2}
+              value={11}
               valueStyle={{ color: '#3f8600' }}
               prefix={<ArrowUpOutlined />}
               suffix="人"
